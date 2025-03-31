de78ddc65e5aa5a0ee44c7d7b31376a31fd3463b
Andreas Schmidt <142411560+andreasschmidt7979@users.noreply.github.com>
2025-03-31 15:16:20 +0000

    c20250331

--- src/app/pages/crypto-viewer/crypto-viever.routes.ts
+++
@@ -1,9 +0,0 @@
-// import { Routes } from '@angular/router';
-// import { CoinListComponent } from './components/coin-list/coin-list.component';
-// import { CoinDetailComponent } from './components/coin-detail/coin-detail.component';
-
-// export default [
-//     { path: '', redirectTo: 'coin-list', pathMatch: 'full' },
-//     { path: 'coin-list', component: CoinListComponent },
-//     { path: 'coin-detail/:id', component: CoinDetailComponent }
-// ] as Routes;
