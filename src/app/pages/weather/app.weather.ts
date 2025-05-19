c60bf5015d499d2ae6da589cfa900d888c8be2ea
andreasschmidt7979 <142411560+andreasschmidt7979@users.noreply.github.com>
2025-05-19 12:53:38 +0200

    c20250519 weather

--- src/app/pages/weather/app.weather.ts
+++ src/app/pages/weather/app.weather.ts
@@ -1,49 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { WeatherInterface } from './models/weatherAPI.models';
-import { WeatherAPIService } from './services/weather-api.service';
+import { CommonModule } from '@angular/common';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.weather.html',
-  styleUrls: ['./app.weather.css'],
+  styleUrl: './app.weather.css',
 })
-export class WeatherComponent implements OnInit {
-  constructor(private weatherService: WeatherAPIService) {}
-
-  weatherData?: WeatherInterface;
-  cityName: string = '';
-
-  fetchData() {
-    this.getWeather(this.cityName);
-    this.cityName = '';
-    console.log("333e");
-  }
-
+export class WeatherComponent {
+  lon = '';
+  lat = '';
+  city = '';
+  countryCode = '';
+  weatherData: any = null;
+  forecastData!: any[];
+  constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
-    this.getWeatherByGeoLocation();
+    this.getWeather();
+    this.getForecastData();
   }
-
-  private getWeather(cityName: string) {
-    console.log("555Start");
-    this.weatherService.fetchWeatherData(cityName).subscribe({ next: (response) => {this.weatherData = response; },});
-  }
-
-  private getWeatherByGeoLocation() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        let long = position.coords.longitude;
-        let lat = position.coords.latitude;
-        this.weatherService.getlocation(lat, long).subscribe({
-          next: (res) => {
-            this.getWeather(res.city);
-            this.cityName = '';
-          },
-        });
+  getWeather() {
+    this.weatherService
+      .getWeather(this.city, this.countryCode)
+      .subscribe((data) => {
+        console.log(data); // Debugging purposes
+        this.weatherData = data;
       });
-    }
+  }
+  getForecastData(): void {
+    this.weatherService
+      .getForecastData(this.city, this.countryCode)
+      .subscribe((data) => {
+        console.log(data);
+        this.forecastData = data;
+      });
   }
 }
+
+// import { Component, OnInit } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+// import { WeatherInterface } from './models/weatherAPI.models';
+// import { WeatherAPIService } from './services/weather-api.service';
+
+// @Component({
+//   selector: 'app-weather',
+//   standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './app.weather.html',
+//   styleUrls: ['./app.weather.css'],
+// })
+// export class WeatherComponent implements OnInit {
+//   constructor(private weatherService: WeatherAPIService) {}
+
+//   weatherData?: WeatherInterface;
+//   cityName: string = '';
+
+//   fetchData() {
+//     this.getWeather(this.cityName);
+//     this.cityName = '';
+//     console.log("333e");
+//   }
+
+//   ngOnInit(): void {
+//     this.getWeatherByGeoLocation();
+//   }
+
+//   private getWeather(cityName: string) {
+//     console.log("555Start");
+//     this.weatherService.fetchWeatherData(cityName).subscribe({ next: (response) => {this.weatherData = response; },});
+//   }
+
+//   private getWeatherByGeoLocation() {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition((position) => {
+//         let long = position.coords.longitude;
+//         let lat = position.coords.latitude;
+//         this.weatherService.getlocation(lat, long).subscribe({
+//           next: (res) => {
+//             this.getWeather(res.city);
+//             this.cityName = '';
+//           },
+//         });
+//       });
+//     }
+//   }
+// }
---
+++ src/app/pages/weather/services/weather.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WeatherService {
+  constructor(private http: HttpClient) {}
+  getWeather(city: string, countryCode: string) {
+    // Replace YOUR_API_KEY with your actual API key from OpenWeatherMap
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=5fb663bc385b723bc97ce10fe43141c4`;
+    console.log('today weather');
+    return this.http.get(apiUrl);
+  }
+  getForecastData(city: string, countryCode: string) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city},${countryCode}&units=metric&appid=5fb663bc385b723bc97ce10fe43141c4`;
+    console.log('5day weather');
+    return this.http.get(url).pipe(
+      map((data: any) => {
+        return data.list.filter((item: any) => {
+          return item.dt_txt.includes('12:00:00'); // Filter to include only the 12:00:00 forecast for each day
+        });
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(error: any) {
+    console.error('An error occurred:', error);
+    return throwError(error.message || 'Something went wrong');
+  }
+}
